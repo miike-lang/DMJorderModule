4d7b0748c64e4c3268ecf6ca087aeda868154086
miike-lang <jouw@email.nl>
2026-04-24 22:38:15 +0200

    database url fallback

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+process.env.DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres:fDxMcwpNpIIpVPqfjQwqwqxKhblxPYNo@shuttle.proxy.rlwy.net:48338/railway";
 import express from "express";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
